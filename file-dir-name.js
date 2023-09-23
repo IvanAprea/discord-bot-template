0d6b68c0bd87bccb35d9db8a85adff67c5eba56d
ivan aprea <ivanaprea.cabj@gmail.com>
2023-09-23 20:58:35 -0300

    chore: function to get dirname and filename props in es type

---
+++ file-dir-name.js
@@ -0,0 +1,11 @@
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+export default function fileDirName() {
+
+	const __filename = fileURLToPath(import.meta.url);
+
+	const __dirname = dirname(__filename);
+
+	return { __dirname, __filename };
+}
