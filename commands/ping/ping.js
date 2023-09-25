3741701c925e02a18d986515729867fea0993582
ivan aprea <ivanaprea.cabj@gmail.com>
2023-09-24 23:24:35 -0300

    fix: change ping command to export default

--- commands/ping/ping.js
+++ commands/ping/ping.js
@@ -1,12 +1,12 @@
 import { SlashCommandBuilder } from "discord.js";
 
-module.exports = {
+export default {
 	data: new SlashCommandBuilder()
 		// Command name
 		.setName("ping")
 		// Command description when listing all commands
 		.setDescription("Replies with Pong!"),
-	async execute(interaction) {
+	execute: async (interaction) => {
 		await interaction.reply("Pong!");
 	},
 };
